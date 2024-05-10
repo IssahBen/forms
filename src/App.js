68520438f92d63a074195673ddf848e4af59d062
IssahBen <benedictissah@gmail.com>
2024-05-10 10:51:12 -0500

    Multiform Complete

--- src/App.js
+++ src/App.js
@@ -1,95 +1,115 @@
+import { useState } from "react";
+import Page1 from "./page1";
+import Page2 from "./page2";
+import Page3 from "./page3";
+import Header from "./header";
+import Navbar from "./navbar";
+import Page4 from "./page4";
 export default function App() {
+  const [pageNumber, setPageNumber] = useState(1);
+  const [submitted, setSubmitted] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [plan, setPlan] = useState("");
+  const [addons, setAddons] = useState([]);
+
+  let planArr = plan.split(",");
+
+  let addArr = addons.map((add) => {
+    const [x, y] = add.split(",");
+    return { name: x, price: y };
+  });
+
+  let total = 0;
+  addArr.forEach((obj) => (total += Number(obj.price)));
+  total += Number(planArr[1]);
+  function handleChange() {
+    setPageNumber(1);
+  }
+  function handlePageSelect(e) {
+    const btn = e.target;
+    if (Number(btn.value) === -1 && pageNumber > 1) {
+      setPageNumber((num) => (num += -1));
+    }
+    if (Number(btn.value) === 1 && pageNumber < 4) {
+      setPageNumber((num) => (num += 1));
+    }
+  }
+
+  function handleSubmit() {
+    if (
+      name.trim() !== "" &&
+      email.trim() !== "" &&
+      number.trim() !== "" &&
+      plan.trim() !== "" &&
+      addons.size !== 0
+    ) {
+      setSubmitted(true);
+    }
+  }
+
+  function planSelect(e) {
+    if (e.target.nodeName === "BUTTON") {
+      setPlan(e.target.value);
+    }
+  }
+
+  function handleAddons(e) {
+    if (addons.includes(e.target.value)) {
+      const element = e.target;
+      element.checked = false;
+    } else {
+      const element = e.target;
+      element.checked = true;
+    }
+    setAddons((adds) =>
+      adds.includes(e.target.value)
+        ? (adds = adds.filter((adds) => adds !== e.target.value))
+        : (adds = [...addons, e.target.value])
+    );
+  }
+
   return (
     <div className=" h-screen w-screen md:flex  md:justify-center md:items-center bg-pastel_blue">
-      <div className=" w-full md:w-10/12 md:h-2/3 flex flex-col md:flex-row bg-white md:p-4 rounded-2xl">
-        <div className="w-full md:w-1/3 h-full">
-          <Header />
+      <div className=" w-full md:w-10/12 md:h-2/3 flex flex-col md:flex-row h-full bg-white md:p-4 rounded-2xl space-y-0">
+        <div className="w-full md:w-1/3  md:h-full h-1/3  bg-mobile-sidebar md:bg-desktop-sidebar bg-no-repeat bg-cover md:bg-cover  md:rounded-2xl ">
+          <Header Number={pageNumber} />
         </div>
-        <div className="md:w-2/3 md:p-8">
-          <Page1 />
-          <Page2 />
-          <Page3 />
-          <Page4 />
-          <Navbar />
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function Page1() {
-  return (
-    <div className="w-full h-full bg-white">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis natus
-      quas pariatur velit nam. Vitae saepe blanditiis est. Nam corrupti
-      molestiae aut blanditiis obcaecati! Rerum, reprehenderit ipsum. A,
-      cupiditate numquam.
-    </div>
-  );
-}
-
-function Header() {
-  return (
-    <div className=" w-full  h-96 md:h-full bg-mobile-sidebar md:bg-desktop-sidebar bg-no-repeat bg-contain md:bg-cover md:rounded-2xl  md:pl-2 md:pr-10">
-      <div className="w-full  flex justify-center items-center">
-        <div className="flex mt-10 md:flex-col md:space-x-0 space-x-4 md:space-y-8">
-          <div className="flex space-x-4 ">
-            <button className="rounded-full text-white text-lg font-bold font-sans border  md:px-4 px-3 py-1 border-white">
-              1
-            </button>
-            <div className="hidden md:flex flex-col">
-              <label htmlFor="" className="font-thin text-cool_gray font-sans">
-                Step 1
-              </label>
-              <p className="font-bold font-sans text-sm text-white">
-                YOUR INFO
-              </p>
-            </div>
-          </div>
-          <div className="flex space-x-4">
-            <button className="rounded-full border md:px-4 px-3 py-1 border-white  text-white text-lg font-bold font-sans">
-              2
-            </button>
-            <div className="hidden md:flex flex-col">
-              <label htmlFor="" className="font-thin text-cool_gray font-sans">
-                Step 2
-              </label>
-              <p className="font-bold font-sans text-sm text-white">
-                SELECT PLAN
-              </p>
-            </div>
-          </div>
-          <div className="flex space-x-4">
-            <button className="rounded-full border md:px-4 px-3 py-1 border-white  text-white text-lg font-bold font-sans">
-              3
-            </button>
-            <div className="hidden md:flex flex-col">
-              <label htmlFor="" className="font-thin text-cool_gray font-sans">
-                Step 3
-              </label>
-              <p className="font-bold font-sans text-sm text-white">ADD-ONS</p>
-            </div>
-          </div>
-          <div className="flex space-x-4">
-            <button className="rounded-full border md:px-4 px-3 py-1  border-white  text-white text-lg font-bold font-sans">
-              4
-            </button>
-            <div className="hidden md:flex flex-col">
-              <label htmlFor="" className="font-thin text-cool_gray font-sans">
-                Step 4
-              </label>
-              <p className="font-semibold font-sans text-sm text-white">
-                SUMMARY
-              </p>
-            </div>
-          </div>
+        <div className=" flex flex-col   relative md:w-2/3  w-full  h-full md:p-8  bg-pastel_blue md:bg-white">
+          {pageNumber === 1 ? (
+            <Page1
+              name={name}
+              setName={setName}
+              email={email}
+              setEmail={setEmail}
+              number={number}
+              setNumber={setNumber}
+            />
+          ) : (
+            ""
+          )}
+          {pageNumber === 2 ? <Page2 setPlan={planSelect} plan={plan} /> : ""}
+          {pageNumber === 3 ? <Page3 add={handleAddons} addons={addons} /> : ""}
+          {pageNumber === 4 ? (
+            <Page4
+              submitted={submitted}
+              planArr={planArr}
+              addArr={addArr}
+              total={total}
+              onChange={handleChange}
+            />
+          ) : (
+            ""
+          )}
+          <Navbar
+            pageNumber={pageNumber}
+            navigate={handlePageSelect}
+            submitted={submitted}
+            handleSubmit={handleSubmit}
+          />
         </div>
       </div>
     </div>
   );
 }
-
-function Page2() {}
-function Page3() {}
-function Page4() {}
-function Navbar() {}
